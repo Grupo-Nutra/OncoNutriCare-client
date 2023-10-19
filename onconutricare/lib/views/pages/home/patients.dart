1af1084b6f9d838a1ee8cde7797f9cae124b02db
pdaniel37 <pedrodaniel.unb@gmail.com>
2023-10-18 22:33:34 -0300

    feat(#12): adicionando abas na home

---
+++ onconutricare/lib/views/pages/home/patients.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PatientsPage extends StatefulWidget {
+  const PatientsPage({super.key});
+
+  @override
+  State<PatientsPage> createState() => _PatientsPageState();
+}
+
+class _PatientsPageState extends State<PatientsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: Placeholder(),
+    );
+  }
+}
